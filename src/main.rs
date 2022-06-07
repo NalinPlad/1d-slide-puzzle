d647be91a1aa629d5f8d9a9775857a3c0dbd378b
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-06-06 20:56:34 -0700

    cursor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+extern crate colored;
+
 use std::io::{stdin, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use std::process;
 use rand::distributions::{Distribution, Uniform};
+use colored::*;
+
 
 fn swap(inp: &[i8; 6], mv: i8) -> [i8; 6] {    
     match mv {
@@ -36,7 +40,7 @@ fn main() {
     //setting up stdout and going into raw mode
     let mut stdout = stdout().into_raw_mode().unwrap();
 
-    write!(stdout, r#"{}{}1,2,3 to swap keys. CTRL-S to (s)cramble. CTRL-C to exit. Press enter to start"#, termion::cursor::Goto(1, 1), termion::clear::All)
+    write!(stdout, r#"{}{}{}1,2,3 to swap keys. CTRL-S to (s)cramble. CTRL-C to exit. Press enter to start"#, termion::cursor::Hide, termion::cursor::Goto(1, 1), termion::clear::All)
             .unwrap();
     stdout.flush().unwrap();
 
@@ -57,11 +61,15 @@ fn main() {
             Key::Char('2') => cube = swap(&cube,1),
             Key::Char('3') => cube = swap(&cube,2),
 
-            Key::Ctrl('c') => process::exit(0), // Exit
+            Key::Ctrl('c') => {
+                write!(stdout, r#"{}"#, termion::cursor::Show);
+                process::exit(0);
+            }, // Exit
             Key::Ctrl('s') => {
                 let between = Uniform::from(0 .. 3);
                 let mut rng = rand::thread_rng();
 
+                // Scramble
                 for _c in 1 .. 10000{
                     let throw = between.sample(&mut rng);
                     
@@ -72,11 +80,19 @@ fn main() {
                         i32::MIN..=-1_i32 | 3_i32..=i32::MAX => todo!()
                     }
                 }
-            }, // Scramble
+            },
             _ => (),
         }
         for key in cube {
-            print!(".{key}.");
+            match key {
+                1 => print!("{}{}{}"," ".to_string().on_red() ,key.to_string().on_red(), " ".to_string().on_red()),
+                2 => print!("{}{}{}"," ".to_string().on_green() ,key.to_string().on_green(), " ".to_string().on_green()),
+                3 => print!("{}{}{}"," ".to_string().on_yellow() ,key.to_string().on_yellow(), " ".to_string().on_yellow()),
+                4 => print!("{}{}{}"," ".to_string().on_cyan() ,key.to_string().on_cyan(), " ".to_string().on_cyan()),
+                5 => print!("{}{}{}"," ".to_string().on_blue() ,key.to_string().on_blue(), " ".to_string().on_blue()),
+                6 => print!("{}{}{}"," ".to_string().on_magenta() ,key.to_string().on_magenta(), " ".to_string().on_magenta()),
+                i8::MIN..=0_i8 | 6_i8..=i8::MAX => todo!()
+            }
         }
         stdout.flush().unwrap();
     }
