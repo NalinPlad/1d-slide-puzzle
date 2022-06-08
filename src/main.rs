0b01e9c3da298ae7f42f8d3427e790a6f6f2e18e
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-06-07 20:12:35 -0700

    forrmattting update

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,40 @@
+extern crate ansi_rgb;
 extern crate colored;
 extern crate rgb;
-extern crate ansi_rgb;
 
+use ansi_rgb::{Background, Foreground};
+use rand::distributions::{Distribution, Uniform};
+use rgb::RGB8;
 use std::io::{stdin, stdout, Write};
+use std::process;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use std::process;
-use rand::distributions::{Distribution, Uniform};
-use ansi_rgb::{ Foreground, Background };
-use rgb::RGB8;
-
 
 fn swap(inp: &[i8; 6], mv: i8) -> [i8; 6] {
     match mv {
         // def = [0,1,2,3,4,5]
         0 => {
-            let fl: [i8; 6] = [inp[3],inp[2],inp[1],inp[0],inp[4],inp[5]];
-            return fl;
-        },
-
-        1 => {
-            let fl: [i8; 6] = [inp[0],inp[4],inp[3],inp[2],inp[1],inp[5]];
-            return fl;
-        },
-
-        2 => {
-            let fl: [i8; 6] = [inp[0],inp[1],inp[5],inp[4],inp[3],inp[2]];
+            let fl: [i8; 6] = [inp[3], inp[2], inp[1], inp[0], inp[4], inp[5]];
             return fl;
         }
 
-        i8::MIN..=-1_i8 | 3_i8..=i8::MAX => todo!()
+        1 => {
+            let fl: [i8; 6] = [inp[0], inp[4], inp[3], inp[2], inp[1], inp[5]];
+            return fl;
+        }
+
+        2 => {
+            let fl: [i8; 6] = [inp[0], inp[1], inp[5], inp[4], inp[3], inp[2]];
+            return fl;
+        }
+
+        i8::MIN..=-1_i8 | 3_i8..=i8::MAX => todo!(),
     }
 }
 
-
-
 fn main() {
-    let mut cube: [i8; 6] = [1,2,3,4,5,6];
+    let mut cube: [i8; 6] = [1, 2, 3, 4, 5, 6];
     let mut moves: i64 = 0;
 
     let mut can_solve: bool = false;
@@ -55,13 +52,12 @@ fn main() {
     //setting up stdout and going into raw mode
     let mut stdout = stdout().into_raw_mode().unwrap();
 
-    write!(stdout, r#"{}{}{}1,2,3 to swap keys. CTRL-S to (s)cramble. CTRL-C to ex(c)it. Press ENTER to start"#,
+    write!(stdout, r#"{}{}{}1,2,3 to swap keys. CTRL-S to (s)cramble. CTRL-C to exit. Press ENTER to start"#,
      termion::cursor::Hide,
       termion::cursor::Goto(1, 1),
        termion::clear::All)
             .unwrap();
     stdout.flush().unwrap();
-
 
     //detecting keydown events
     for c in stdin.keys() {
@@ -75,42 +71,50 @@ fn main() {
         .unwrap();
 
         match c.unwrap() {
-            Key::Char('1') => {cube = swap(&cube,0); moves += 1;},
-            Key::Char('2') => {cube = swap(&cube,1); moves += 1;},
-            Key::Char('3') => {cube = swap(&cube,2); moves += 1;},
+            Key::Char('1') => {
+                cube = swap(&cube, 0);
+                moves += 1;
+            }
+            Key::Char('2') => {
+                cube = swap(&cube, 1);
+                moves += 1;
+            }
+            Key::Char('3') => {
+                cube = swap(&cube, 2);
+                moves += 1;
+            }
 
             Key::Ctrl('c') => {
                 write!(stdout, r#"{}"#, termion::cursor::Show).unwrap();
                 process::exit(0);
-            }, // Exit
+            } // Exit
             Key::Ctrl('s') => {
-                let between = Uniform::from(0 .. 3);
+                let between = Uniform::from(0..3);
                 let mut rng = rand::thread_rng();
 
                 // Scramble
-                for _c in 1 .. 3{
+                for _c in 1..3 {
                     let throw = between.sample(&mut rng);
-                    
                     match throw {
-                        0 => cube = swap(&cube,0),
-                        1 => cube = swap(&cube,1),
-                        2 => cube = swap(&cube,2),
-                        i32::MIN..=-1_i32 | 3_i32..=i32::MAX => todo!()
+                        0 => cube = swap(&cube, 0),
+                        1 => cube = swap(&cube, 1),
+                        2 => cube = swap(&cube, 2),
+                        i32::MIN..=-1_i32 | 3_i32..=i32::MAX => todo!(),
                     }
                 }
 
                 can_solve = true;
                 moves = 0;
-            },
+            }
             _ => (),
         }
-        if can_solve && cube == [1,2,3,4,5,6]{
+        if can_solve && cube == [1, 2, 3, 4, 5, 6] {
             if moves < 10 {
-                write!(stdout,r#"SOLVED: {} MOVES!"#, moves.to_string().fg(green)).unwrap();
+                write!(stdout, r#"SOLVED: {} MOVES!"#, moves.to_string().fg(green)).unwrap();
             } else if moves < 25 {
-                write!(stdout,r#"SOLVED: {} MOVES!"#, moves.to_string().fg(yellow)).unwrap();
+                write!(stdout, r#"SOLVED: {} MOVES!"#, moves.to_string().fg(yellow)).unwrap();
             } else {
-                write!(stdout,r#"SOLVED: {} MOVES!"#, moves.to_string().fg(red)).unwrap();
+                write!(stdout, r#"SOLVED: {} MOVES!"#, moves.to_string().fg(red)).unwrap();
             }
             stdout.flush().unwrap();
             can_solve = false;
@@ -125,20 +129,47 @@ fn main() {
             }
         }
 
-
-
         for key in cube {
             match key {
-                1 => print!("{}{}{}","  ".to_string().bg(red) ,key.to_string().bg(red).fg(black), "  ".to_string().bg(red)),
-                2 => print!("{}{}{}","  ".to_string().bg(orange) ,key.to_string().bg(orange).fg(black), "  ".to_string().bg(orange)),
-                3 => print!("{}{}{}","  ".to_string().bg(yellow) ,key.to_string().bg(yellow).fg(black), "  ".to_string().bg(yellow)),
-                4 => print!("{}{}{}","  ".to_string().bg(green) ,key.to_string().bg(green).fg(black), "  ".to_string().bg(green)),
-                5 => print!("{}{}{}","  ".to_string().bg(blue) ,key.to_string().bg(blue).fg(black), "  ".to_string().bg(blue)),
-                6 => print!("{}{}{}","  ".to_string().bg(violet) ,key.to_string().bg(violet).fg(black), "  ".to_string().bg(violet)),
-                _ => ()
+                1 => print!(
+                    "{}{}{}",
+                    "  ".to_string().bg(red),
+                    key.to_string().bg(red).fg(black),
+                    "  ".to_string().bg(red)
+                ),
+                2 => print!(
+                    "{}{}{}",
+                    "  ".to_string().bg(orange),
+                    key.to_string().bg(orange).fg(black),
+                    "  ".to_string().bg(orange)
+                ),
+                3 => print!(
+                    "{}{}{}",
+                    "  ".to_string().bg(yellow),
+                    key.to_string().bg(yellow).fg(black),
+                    "  ".to_string().bg(yellow)
+                ),
+                4 => print!(
+                    "{}{}{}",
+                    "  ".to_string().bg(green),
+                    key.to_string().bg(green).fg(black),
+                    "  ".to_string().bg(green)
+                ),
+                5 => print!(
+                    "{}{}{}",
+                    "  ".to_string().bg(blue),
+                    key.to_string().bg(blue).fg(black),
+                    "  ".to_string().bg(blue)
+                ),
+                6 => print!(
+                    "{}{}{}",
+                    "  ".to_string().bg(violet),
+                    key.to_string().bg(violet).fg(black),
+                    "  ".to_string().bg(violet)
+                ),
+                _ => (),
             }
         }
         stdout.flush().unwrap();
-}
-
+    }
 }
